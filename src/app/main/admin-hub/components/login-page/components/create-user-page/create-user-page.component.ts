f23f9589af9cd2166dd36bed2aba293f8da8b814
Andrew Moraes <andrewmoraesds@gmail.com>
2021-08-30 15:34:32 -0300

    add formbuilder na create user e novas validacoes

--- src/app/main/admin-hub/components/login-page/components/create-user-page/create-user-page.component.ts
+++ src/app/main/admin-hub/components/login-page/components/create-user-page/create-user-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user-page',
@@ -6,21 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user-page.component.scss']
 })
 export class CreateUserPageComponent implements OnInit {
-  cnpja = ''
+
+  public registerForm: FormGroup = this.formBuilder.group({
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+    name: ['', Validators.required],
+    dateBirth: ['', Validators.required]
+  });
+
   public selectList = [
-    {id: 0, name: 'Cadastrar uma nova'},
-    {id: 1, name: 'Teste A'},
-    {id: 2, name: 'Teste B'}
+    { id: 0, name: 'Cadastrar uma nova' },
+    { id: 1, name: 'Teste A' },
+    { id: 2, name: 'Teste B' }
   ];
 
   public selectedId = -1;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  // conseguir mais fácil dados do form
+  get f() { return this.registerForm.controls; }
+
   public changeSelectedCompany(e: any) {
-     this.selectedId = e.target.value;
+    this.selectedId = e.target.value;
+  }
+
+  public isPasswordValid(): Boolean {
+    if(this.f.password.touched && this.f.password.valid && this.f.confirmPassword.touched && this.f.confirmPassword.valid){
+      return this.f.password.value === this.f.confirmPassword.value;
+    }
+    return true;
   }
 }
